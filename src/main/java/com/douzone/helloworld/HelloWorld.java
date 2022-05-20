8099a72d08b9753a7b86455a5d475814cbe78fc5
김현석 <gustjr324@gmail.com>
2022-05-20 16:31:21 +0900

    ignore

---
+++ src/main/java/com/douzone/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.douzone.helloworld;
+import java.util.Arrays;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String str = "How are you";
+		// 문자열 -> char 배열로 변환
+		char[] charArr = str.toCharArray();
+		// char 배열 출력
+		System.out.println(charArr);
+
+	}
+}
