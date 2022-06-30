2b58c4257757c12dd343523a0c37ce458618a1e9
김현석 <gustjr324@gmail.com>
2022-06-30 10:07:02 +0900

    test

---
+++ src/main/java/com/douzone/helloworld/aaa.java
@@ -0,0 +1,14 @@
+package com.douzone.helloworld;
+
+public class aaa {
+
+	public static void main(String[] args) {
+		System.out.println("5 + 2 = " + 3 + 4);
+		System.out.println("5 + 2 = " + (3 + 4));
+		String a = "아" + 2;
+		a += "가나" + 2;
+		a += 2;
+		System.out.println(a);
+	}
+
+}
